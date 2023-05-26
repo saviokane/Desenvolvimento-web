e617403acaac9bd8c6d5961f548a8c3987993bd4
LeonardoOsugi <leoosugi96@gmail.com>
2023-05-26 11:25:50 -0300

    Parte 2 JS

---
+++ JavaScript/index.js
@@ -0,0 +1,12 @@
+let cartao;
+
+alert("Atualize seu windows !!!");
+cartao = prompt("Atualize teu nevegado");
+confirm("Voce realmente confirma?");
+
+document.write("Seu cartao: "+cartao);
+
+let minha_div = document.querySelector("div");
+document.querySelector("div");
+
+minha_div.innerHTML = "<p>DIV MODIFICADA COM SUCESSO</p>";
